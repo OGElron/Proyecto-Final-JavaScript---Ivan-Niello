982cf28466f4380a1c2cad1d5fc4185598c564cc
Ivan Dario Niello <ivaniello@hotmail.com>
2022-04-28 01:05:38 -0300

    segunda entrega

---
+++ js/app.js
@@ -0,0 +1,162 @@
+//------- carrito
+
+/*Entidades*/
+class LogIn {
+    constructor(username, email, password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
+/*Variables*/
+//login
+userLogged = [];
+
+let accessz = document.getElementById("form_regis");
+
+const tiendita = [
+    {item: "remera1",categoria: "remera",precio:1300,stock:5}, 
+    {item: "remera2",categoria: "remera",precio:1300,stock: 12},
+    {item: "remera3",categoria: "remera",precio:1400,stock: 11},
+    {item: "remera4",categoria: "remera",precio:1400,stock: 13},
+    {item: "gorra1",categoria: "gorra",precio:990,stock:8},
+    {item: "gorra2",categoria: "gorra",precio:990,stock: 6},
+    {item: "taza1",categoria: "taza",precio:650,stock: 20},
+    {item: "taza2",categoria: "taza",precio:650,stock:23},
+    {item: "mousepad1",categoria: "mousepad",precio:750,stock:0},
+    {item: "mousepad2",categoria: "mousepad",precio:750,stock: 3}
+]
+
+let precioTotal = 0
+
+const contenedorCards = document.getElementById("contenedorCards")
+
+/*Funciones*/
+//login
+const logInz = () => {
+    let username = document.querySelector("#login_nombre").value;
+    let email = document.querySelector("#login_correo").value; 
+    let password = document.querySelector("#login_contraseña").value;
+
+    const nuevoUser = new LogIn (username, email, password);
+    // userLogged.push(nuevoUser);
+
+    let listaUsers = [];
+    if(localStorage.getItem("userLogged") != null) {
+        listaUsers = JSON.parse(localStorage.getItem("userLogged"));
+        listaUsers.push(nuevoUser);
+        localStorage.setItem("userLogged", JSON.stringify(listaUsers));
+    } else { 
+        userLogged.push(nuevoUser);
+        localStorage.setItem("userLogged", JSON.stringify(listaUsers));
+    } 
+    console.log(nuevoUser)
+    return nuevoUser;
+}
+
+/*Event*/
+
+function Acceder (e) {
+    e.preventDefault();
+    logInz();
+}
+accessz.addEventListener("submit", Acceder)
+
+//creador de cards para la tienda
+
+const cardYbuttons=(array) => {
+
+    contenedorCards.innerHTML=""
+
+    array.forEach((tiendita)=> {
+
+        const divCard = document.createElement('div');
+
+        divCard.classList.add('cards')
+
+         divCard.innerHTML = `
+                            <h4 class="nombre_item"> ${tiendita.item}</h4>
+
+                            <p class= "categoria_item"> ${tiendita.categoria} </p>
+
+                            <p class= "precio_item"> ${tiendita.precio} </p>
+
+                            <button id= "btnCompra"> Comprar </button>
+
+                            `
+        contenedorCards.append(divCard)
+        
+        //boton comprar
+        // const comprarBoton = document.getElementById(`comprar${tiendita.item}`)
+
+        // comprarBoton.addEventListener('click', ()=> {
+
+        //     comprarProducto()
+
+        // })
+
+    })
+
+}
+
+//-----filtros
+const filtroDefault = document.getElementById("default")
+
+const filtro1500 = document.getElementById("mn1500")
+
+const filtro1000 = document.getElementById("mn1000")
+
+const filtro750 = document.getElementById("mn750")
+
+const filtroDefault2 = document.getElementById("default")
+
+const filtroRemeras = document.getElementById("remeras")
+
+const filtroGorras = document.getElementById("gorras")
+
+const filtroTazas = document.getElementById("tazas")
+
+const filtroMspads = document.getElementById("mspads")
+
+const filtroOtros = document.getElementById("otros")
+
+filtroDefault.addEventListener("click", () => {
+    cardYbuttons(tiendita);
+})
+
+filtro1500.addEventListener("click", () => {
+    const filtroMenor1500 = tiendita.filter (el=>el.precio < 1500);
+    cardYbuttons(filtroMenor1500);
+})
+
+filtro1000.addEventListener("click", () => {
+    const filtroMenor1000 = tiendita.filter (el=>el.precio < 1000);
+    cardYbuttons(filtroMenor1000);
+})
+
+filtro750.addEventListener("click", () => {
+    const filtroMenor750 = tiendita.filter (el=>el.precio < 750);
+    cardYbuttons(filtroMenor750);
+})
+
+filtroDefault2.addEventListener("click", () => {
+    cardYbuttons(tiendita);
+})
+
+filtroRemeras.addEventListener("click", () => {
+    const filtroRemeras = tiendita.filter(el => el.categoria === "remera")
+    cardYbuttons(filtroRemeras)
+})
+filtroGorras.addEventListener("click", () => {
+    const filtroGorras = tiendita.filter(el => el.categoria === "gorra")
+    cardYbuttons(filtroGorras)
+})
+filtroTazas.addEventListener("click", () => {
+    const filtroTazas = tiendita.filter(el => el.categoria === "taza")
+    cardYbuttons(filtroTazas)
+})
+filtroMspads.addEventListener("click", () => {
+    const filtroMspads = tiendita.filter(el => el.categoria === "mousepad")
+    cardYbuttons(filtroMspads)
+})
+
--- js/main.js
+++ js/main.js
@@ -1,12 +1,5 @@
 // carrito de compras de un negocio
 /*Entidades*/
-class LogIn {
-    constructor(username, email, password) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
-}
 class Producto {
     constructor(item, categoria, precio, stock) {
         this.item = item;
@@ -16,11 +9,7 @@ class Producto {
     }
 }
 /*Variables*/
-//login
-userLogged = [];
-let accessz = document.getElementById("accesz");
-
-
+let cargar = document.querySelector("#cargar");
 //tienda
 let tiendita = [
     {item: "remera1", categoria: "remera", precio:1300, stock:5}, 
@@ -33,37 +22,9 @@ let tiendita = [
     {item: "taza2", categoria: "taza", precio:650, stock:23},
     {item: "mousepad1", categoria: "varios", precio:750, stock:0},
     {item: "mousepad2", categoria: "varios", precio:750, stock: 3}]
-console.log(tiendita)
 
 /*Funciones*/
-//login
-const logInz = () => {
-    let username = document.querySelector("#login_nombre").value;
-    let email = document.querySelector("#login_correo").value; 
-    let password = document.querySelector("#login_contraseña").value;
 
-    const nuevoUser = new LogIn (username, email, password);
-    // userLogged.push(nuevoUser);
-
-    let listaUsers = [];
-    if(localStorage.getItem("userLogged") != null) {
-        listaUsers = JSON.parse(localStorage.getItem("userLogged"));
-        listaUsers.push(nuevoUser);
-        localStorage.setItem("userLogged", JSON.stringify(listaUsers));
-    } else { 
-        userLogged.push(nuevoUser);
-        localStorage.setItem("userLogged", JSON.stringify(listaUsers));
-    } 
-    console.log(nuevoUser)
-    return nuevoUser;
-}
-let busca2r = tiendita.find(obj=>{
-    return obj.item == search;
-})
-let search = document.querySelector("#busca3");
-
-
-//producto
 const producto = () => {
     let item = document.querySelector("#nuevo_item").value;
     let categoria = document.querySelector("#nuevo_categoria").value; 
@@ -71,67 +32,13 @@ const producto = () => {
     let stock = document.querySelector("#nuevo_stock").value;
 
     const nuevoProducto = new Producto (item, categoria, precio, stock);
-    console.log(nuevoProducto);
+    
     tiendita.push(nuevoProducto);
 }
 
-const comprarProducto =(cantidad) => {
-    if (this.stock > cantidad) {
-        this.stock - cantidad;
-        console.log(this.stock);
-        } else {alert("En este momento no tenemos stock. Intente más tarde o contactenos")}
-        console.log(this.stock);
-    }
-   
-const filtroMenor1500 = tiendita.filter (el=>el.precio < 1500); 
-const filtroMenor1000 = tiendita.filter (el=>el.precio < 1000);
-const filtroMenor750 = tiendita.filter (el=>el.precio < 750);
-
-if (filtroMenor750 = true) {
-    display == filtroMenor750; 
-}
-
-// const oculTar = () => {
-//     let tar = document.querySelectorAll(".tar")
-//     if (tar.prezio.innerText == filtroMenor750){
-//         tar.prezio.add("ocultar")
-//     }
-// } 
-// const filtro3 = document.getElementById("#mn750" != filtroMenor750)
-// filtro3.style.display="hidden";
-
-
-// filtro3.addEventListener("change",(e) => {
-//     e.preventDefault();
-// }
-// filtroMenor750.addEventListener("select", (e)=> {
-// e.oculTar()
-// })
-
 /*Event*/
 
-accessz.addEventListener= ("click",(e) => {
-    e.preventDefault();
-    logInz();
-})
 cargar.addEventListener ("click",(e)=> {
     e.preventDefault();
     producto();
 })
-busca3.addEventListener("click", (e)=> {
-    e.busca2r})
-
-// hace falta hacer algo con esto? ya hace solo la funcion que quiero que cumpla
-//---> limpiar.addEventListener("click", (e))=> {
-
-
-
-/* Alerta, de carrito de compra. alerta que no funciona. 
-Si esta dentro de la class no me marca error, pero no da la alerta. 
-Si esta fuera me marca undefined. 
-
-alert(`Tu carrito: \n
-    Producto: ${item} \n
-    Cantidad: ${cantidad} \n
-    Total: ${precioTotal}`);    
-*/
